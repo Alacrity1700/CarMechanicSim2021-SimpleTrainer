566b0252b7b7ae4630e553a9030f9dc7a6312428
Ivan <91813660+Alacrity1700@users.noreply.github.com>
2024-01-02 17:30:49 -0500

    Update CMS2021Trainer.cpp

--- CMS2021Trainer/CMS2021Trainer.cpp
+++ CMS2021Trainer/CMS2021Trainer.cpp
@@ -3,43 +3,70 @@
 #include "procID.h"
 using namespace std;
 
+//template function allows to overload << operator to print a vector's content
+
+template <typename ele_type> ostream& operator<<(ostream& os, const vector<ele_type>& vect_name) {
+
+    // itr is iterator for each element traversal
+
+    for (auto itr : vect_name) {
+
+        os << itr << " ";
+    }
+
+    return os;
+}
+
 int main()
 {
  
-    DWORD procId = GetProcId(L"Car Mechanic Simulator 2021.exe");
-    cout << "Grabbing Process ID" << endl;
-  
-    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"GameAssembly.dll");
-
-    cout << "Grabbing Module Base" << endl;
-   
+    DWORD procId = GetProcId(L"Car Mechanic Simulator 2021.exe"); 
+    uintptr_t gameAssemblyBase = GetModuleBaseAddress(procId, L"GameAssembly.dll");   
+    uintptr_t unityPlayerBase = GetModuleBaseAddress(procId, L"UnityPlayer.dll");
     HANDLE hProcess = 0;
+
     hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+    uintptr_t gameAssemblyPtr = gameAssemblyBase + 0x028098A8;   
+    uintptr_t unityPlayerPtr = unityPlayerBase + 0x0279D8B8;
 
-    cout << "Opening Process" << endl;
 
-    uintptr_t dynamicPtrBaseAddr = moduleBase + 0x028098A8;
+    std::vector<unsigned int> moneyOffsets = {0x98, 0x0, 0x68, 0xC8, 0xF80, 0xB8, 0x2A0}; // 152, 0, 104, 200, 3968, 184, 672
+    std::vector<unsigned int> scrapOffsets = {0x40, 0x80, 0xDB0, 0xC8, 0xAC0, 0xB8, 0x2AC}; // 64, 128, 3504, 200, 2752, 184, 684
+    std::vector<unsigned int> skillOffsets = {0xB8, 0x0, 0x80, 0xC0, 0x180, 0x178, 0x30  }; // 64, 128, 3504, 200, 2752, 184, 684
+    uintptr_t scrapAddr = FindDMAAddy(hProcess, gameAssemblyPtr, scrapOffsets);
+    uintptr_t moneyAddr = FindDMAAddy(hProcess, gameAssemblyPtr, moneyOffsets);
+    uintptr_t skillAddr = FindDMAAddy(hProcess, unityPlayerPtr, skillOffsets);
 
-    cout << "Getting Ptr Base Address" << endl;
-    
-    std::vector<unsigned int> moneyOffsets = {0x98, 0x0, 0x68, 0xC8, 0xF80, 0xB8, 0x2A0};
-    std::vector<unsigned int> scrapOffsets = {0x40, 0x80, 0xDB0, 0xC8, 0xAC0, 0xB8, 0x2AC};
-    uintptr_t scrapAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, scrapOffsets);
-    uintptr_t moneyAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, moneyOffsets);
-    cout << "Resolving Offsets" << endl;
+#ifdef _DEBUG
+    cerr << "********DEBUG********" << endl;
+    cerr << "gameassembly:    " << gameAssemblyBase << endl;
+    cerr << "unityplayer:     " << unityPlayerBase << endl;
+    cerr << "gameassemblyptr: " << gameAssemblyPtr << endl;
+    cerr << "unityplayerptr:  " << unityPlayerPtr << endl;
+    cerr << "money offsets:   " << moneyOffsets << endl;
+    cerr << "scrap offsets:   " << scrapOffsets << endl;
+    cerr << "skill offsets:   " << skillOffsets << endl;
+    cerr << "money add:       " << moneyAddr << endl;
+    cerr << "scrap add:       " << scrapAddr << endl;
+    cerr << "skill add:       " << skillAddr << endl;
+    cerr << "********DEBUG********\n\n" << endl;
+#endif
+
     bool exitProgram = false;
     do {
-        int scrapVal = 0;
-        int moneyVal = 0;
+        int scrapVal{ 0 };
+        int moneyVal{ 0 };
+        int skillVal{ 0 };
         ReadProcessMemory(hProcess, (BYTE*)moneyAddr, &moneyVal, sizeof(moneyVal), nullptr);
         ReadProcessMemory(hProcess, (BYTE*)scrapAddr, &scrapVal, sizeof(scrapVal), nullptr);
         cout << moneyVal << " is your current money amount." << endl;
         cout << scrapVal << " is your current scrap amount." << endl;
 
-        int changedScrapVal = 1000000;
-        int changedMoneyVal = 1000000;
-        int addrToChange = 0;
-        cout << "Would you like to change your scrap(1) or money(2)? Enter a number:" << endl;
+        int changedScrapVal{ 0 };
+        int changedMoneyVal{ 0 };
+        int changedSkillVal{ 0 };
+        int addrToChange{ 0 };
+        cout << "Would you like to change your:\n1. Scrap\n2. Money\n3. Skill Points" << endl;
         cin >> addrToChange;
         if (addrToChange == 1) {
             cout << "Enter your desired scrap amount:" << endl;
@@ -59,8 +86,17 @@ int main()
 
             cout << moneyVal << " is your new money amount." << endl;
         }
+        else if (addrToChange == 3) {
+            cout << "Enter your desired skill point amount:" << endl;
+            cin >> changedSkillVal;
+            WriteProcessMemory(hProcess, (BYTE*)skillAddr, &changedSkillVal, sizeof(changedSkillVal), nullptr);
+            cout << "Skill points Changed Successfully!" << endl;
+            ReadProcessMemory(hProcess, (BYTE*)skillAddr, &skillVal, sizeof(skillVal), nullptr);
+
+            cout << moneyVal << " is your new skill point amount." << endl;
+        }
         else
-            cout << "Enter a valid input.";
+            cout << "Enter a valid input.\n";
 
         cout << "Do you want to perform another change? (0 for Yes, 1 for No): " << endl;
         cin >> exitProgram;
@@ -68,14 +104,6 @@ int main()
 
     } while (exitProgram == false);
     
-    cout << "Trainer Closing...." << endl;
-
-
-    
-    
-    
-
-
-
+    cout << "Trainer Closing...." << endl;    
     return 0;
 }
